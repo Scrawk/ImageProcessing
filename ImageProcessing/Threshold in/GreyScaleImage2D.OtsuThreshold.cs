1730fee80b76c2fa460694a1f92378107c102fce
Justin <29545213+Scrawk@users.noreply.github.com>
2022-05-26 17:22:01 +0800

    Update GreyScaleImage2D.OtsuThreshold.cs

--- ImageProcessing/Threshold in/GreyScaleImage2D.OtsuThreshold.cs
+++ ImageProcessing/Threshold in/GreyScaleImage2D.OtsuThreshold.cs
@@ -1,1 +1,55 @@
+	
+	@Override
+	public int getThreshold(int[] hist) {
+		h = hist;
+		int K = h.length;
+		makeMeanTables(h);
+
+		double sigma2Bmax = 0;
+		int qMax = -1;
+		int n0 = 0;
+		
+		// examine all possible threshold values q:
+		for (int q = 0; q <= K-2; q++) {
+			n0 = n0 +  h[q]; 
+			int n1 = N - n0;
+			if (n0 > 0 && n1 > 0) {
+				double meanDiff = M0[q] - M1[q];
+				double sigma2B =  meanDiff * meanDiff * n0 * n1; // (1/N^2) has been omitted
+				if (sigma2B > sigma2Bmax) {
+					sigma2Bmax = sigma2B;
+					qMax = q;
+				}
+			}
+		}
+		
+		return qMax;
+	}
+	
+	private void makeMeanTables(int[] h) {
+		int K = h.length;
+		this.M0 = new double[K];
+		this.M1 = new double[K];
+		int n0 = 0;
+		long s0 = 0;
+		for (int q = 0; q < K; q++) {
+			n0 = n0 + h[q];
+			s0 = s0 + q * h[q];
+			M0[q] = (n0 > 0) ? ((double) s0)/n0 : -1;
+		}
+		
+		this.N = n0;
+		
+		int n1 = 0;
+		long s1 = 0;
+		M1[K-1] = 0;
+		for (int q = h.length-2; q >= 0; q--) {
+			n1 = n1 + h[q+1];
+			s1 = s1 + (q+1) * h[q+1];
+			M1[q] = (n1 > 0) ? ((double) s1)/n1 : -1;
+		}
+		
+//		return N;
+	}
+}
 
